a10aca05dbcf3c4319a499511ee6637ee75c70c5
Blaire Baker <bakerblaire@gmail.com>
2020-11-26 21:03:32 -0600

    commit tree fc2a94f22ed2d881df7da43b34f10a008f8d9b12

---
+++ lib/insert.sql
@@ -0,0 +1,43 @@
+INSERT INTO series VALUES (1, "Harry Potter", 1, 1);
+INSERT INTO series VALUES (2, "A Song of Ice and Fire", 2, 2);
+
+INSERT INTO books VALUES (1, "Harry Potter and the Sorcerer's Stone", 1997, 1);
+INSERT INTO books VALUES (2, "Harry Potter and the Chamber of Secrets", 1998, 1);
+INSERT INTO books VALUES (3, "Harry Potter and the Prisoner of Azkaban", 1999, 1);
+INSERT INTO books VALUES (4, "A Game of Thrones", 1996, 2);
+INSERT INTO books VALUES (5, "A Clash of Kings", 1998, 2);
+INSERT INTO books VALUES (6, "A Storm of Swords", 2000, 2);
+
+INSERT INTO characters VALUES (1, "Harry Potter", "Draco dormiens", "Wizard", 1);
+INSERT INTO characters VALUES (2, "Hermoine Granger", "Leviosaaa", "Witch", 1);
+INSERT INTO characters VALUES (3, "Ron Weasley", "Don't let it worry you.", "Wizard", 1);
+INSERT INTO characters VALUES (4, "Draco Malfoy", "Purity will always conquer", "Wizard", 1);
+INSERT INTO characters VALUES (5, "Tyrion Lannister", "I drink and I know things", "Human", 2);
+INSERT INTO characters VALUES (6, "Jon Snow", "Sometimes there is no happy choice", "Human", 2);
+INSERT INTO characters VALUES (7, "Ygritte", "You know nothin', Jon Snow.", "Human", 2);
+INSERT INTO characters VALUES (8, "Cersei Lannister", "A Lannister always pays his debts", "Human", 2);
+
+INSERT INTO subgenres VALUES (1, "Young Adult");
+INSERT INTO subgenres VALUES (2, "High Fantasy");
+
+INSERT INTO authors VALUES (1, "JK Rowling");
+INSERT INTO authors VALUES (2, "George R. R. Martin");
+
+INSERT INTO character_books VALUES (1, 1, 1);
+INSERT INTO character_books VALUES (2, 1, 2);
+INSERT INTO character_books VALUES (3, 1, 3);
+INSERT INTO character_books VALUES (4, 2, 1);
+INSERT INTO character_books VALUES (5, 2, 2);
+INSERT INTO character_books VALUES (6, 2, 3);
+INSERT INTO character_books VALUES (7, 3, 1);
+INSERT INTO character_books VALUES (8, 4, 1);
+INSERT INTO character_books VALUES (9, 5, 4);
+INSERT INTO character_books VALUES (10, 5, 5);
+INSERT INTO character_books VALUES (11, 5, 6);
+INSERT INTO character_books VALUES (12, 6, 4);
+INSERT INTO character_books VALUES (13, 6, 5);
+INSERT INTO character_books VALUES (14, 6, 6);
+INSERT INTO character_books VALUES (15, 7, 5);
+INSERT INTO character_books VALUES (16, 8, 4);
+
+
